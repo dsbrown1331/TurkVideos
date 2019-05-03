80b24f826957aaf37de2d98f0cf20117d8b12f72
Daniel Brown <dsbrown1331@gmail.com>
2019-05-03 14:03:43 -0500

    updating accuracy code

--- evaluateTurkerAccuracyCropped.py
+++ evaluateTurkerAccuracyCropped.py
@@ -1,4 +1,5 @@
 import argparse
+import numpy as np
 #parse checkpoint_to_score file
 def parse_checkpoint_to_scores(filename):
     reader = open(filename)
@@ -19,26 +20,31 @@ def parse_checkpoint_to_scores(filename):
 def parse_turk_responses(filename, env_name):
     reader = open(filename)
     count = 0
-    ranking = {} #store as pair of checkpoints and A or B or Not Sure for preferences
+    ranking = [] #store as tupes: pair of checkpoints and A or B or Not Sure for preferences
     for line in reader:
         count += 1
         if count == 1:
             continue #skip header line
         #split on : as delimiter
         parsed = line.split(",")
-        video_a = parsed[-5]
-        video_b = parsed[-4]
+        #print(parsed)
+        video_a = parsed[-6]
+        video_b = parsed[-5]
         label = parsed[-3]
+        #print(video_a)
+        #print(video_b)
+        #print(label)
+        #input()
         #extract checkpoints and MTurk label
         idx_a_env = video_a.find(env_name+"_")
-        idx_a_mp4 = video_a.find(".mp4")
+        idx_a_mp4 = video_a.find("_crop.webm")
         checkpoint_a = video_a[idx_a_env+len(env_name + "_") : idx_a_mp4]
         idx_b_env = video_b.find(env_name+"_")
-        idx_b_mp4 = video_b.find(".mp4")
+        idx_b_mp4 = video_b.find("_crop.webm")
         checkpoint_b = video_b[idx_b_env+len(env_name + "_") : idx_b_mp4]
         print(checkpoint_a, checkpoint_b, parsed[-3])
         #add to ranking dict
-        ranking[(checkpoint_a, checkpoint_b)] = label
+        ranking.append(((checkpoint_a, checkpoint_b), label))
     print(ranking)
     print(count - 1, "lines of turker responses parsed")
     
@@ -51,12 +57,12 @@ def evaluate_accuracy(checkpoint_to_scores, ranking_dict):
     #use the ground truth returns from checkpont_to_scores to evalute the acuracy of MTurk
     correct = 0
     incorrect = 0
-    for r in ranking_dict:
+    for r, label in ranking_dict:
         print("-"*10)
         #find scores
         checkpoint_a = r[0]
         checkpoint_b = r[1]
-        mturk_label = ranking_dict[r]
+        mturk_label = label
         print("checkpoints", checkpoint_a, checkpoint_b)
         print("MTurk label", mturk_label)
         score_a = checkpoint_to_scores[r[0]]
@@ -81,6 +87,75 @@ def evaluate_accuracy(checkpoint_to_scores, ranking_dict):
             correct += 1
             
     return correct / len(ranking_dict), incorrect / len(ranking_dict)
+    
+#evaluate Turker accuracy
+def evaluate_accuracy_majority(checkpoint_to_scores, ranking_dict):
+    #use the ground truth returns from checkpont_to_scores to evalute the acuracy of MTurk
+    correct = 0
+    incorrect = 0
+    #keep a dictionary of checkpoints and labels to parse to get majority votes
+    vote_dict = {}
+    for r, label in ranking_dict:
+        print("-"*10)
+        #find scores
+        checkpoint_a = r[0]
+        checkpoint_b = r[1]
+        mturk_label = label
+        
+        score_a = checkpoint_to_scores[r[0]]
+        score_b = checkpoint_to_scores[r[1]]
+        if score_a > score_b:
+            #flip checkpoints and scores
+            temp = checkpoint_a
+            checkpoint_a = checkpoint_b
+            checkpoint_b = temp
+            temp_s = score_a
+            score_a = score_b
+            score_b = temp_s
+            if mturk_label == "A":
+                mturk_label = "B"
+            elif mturk_label == "B":
+                mturk_label = "A"
+        if (checkpoint_a, checkpoint_b) not in vote_dict:
+            vote_dict[(checkpoint_a, checkpoint_b)] = [0,0,0]        
+        
+        print("incrementing")
+        print("before", vote_dict[(checkpoint_a, checkpoint_b)])
+        if mturk_label == "A": #increment index 0
+            vote_dict[(checkpoint_a, checkpoint_b)][0]+=1
+        elif mturk_label =="B": #increment index 1
+            vote_dict[(checkpoint_a,checkpoint_b)][1]+=1
+        else:
+            vote_dict[(checkpoint_a, checkpoint_b)][2]+=1
+    
+        print("after", vote_dict[(checkpoint_a, checkpoint_b)])
+        
+        
+        print("checkpoints", checkpoint_a, checkpoint_b)
+        print("MTurk label", mturk_label)    
+        print("scores", score_a, score_b)
+            
+    count = 0
+    for pair in vote_dict:
+        #check if majority vote is right
+        maj_vote_idxs = np.argwhere(vote_dict[pair] == np.amax(vote_dict[pair])).flatten().tolist()
+        #if tie then don't consider it
+        if len(maj_vote_idxs) > 1:
+            continue
+        #skip if unsure
+        if maj_vote_idxs[0] == 2:
+            continue
+        else:
+            count += 1
+            print(pair, vote_dict[pair])
+            #check if correct, by design answer B is always correct
+            if maj_vote_idxs[0] == 1:
+                correct += 1
+            else:
+                incorrect += 1
+                
+            
+    return correct / count, incorrect / count
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser(description=None)
@@ -93,7 +168,8 @@ if __name__=="__main__":
     turk_batch_filename = args.turk_batch #"Batch_3624797_batch_results.csv"
     checkpoints_to_scores = parse_checkpoint_to_scores(checkpoint_filename)
     ranking_dict = parse_turk_responses(turk_batch_filename, env_name)
-    pos_accuracy, neg_accuracy = evaluate_accuracy(checkpoints_to_scores, ranking_dict)
+    print(ranking_dict)
+    pos_accuracy, neg_accuracy = evaluate_accuracy_majority(checkpoints_to_scores, ranking_dict)
     print("% correct = ", pos_accuracy)
     print("% incorrect = ", neg_accuracy)
     print("% not sure = ", 1 - pos_accuracy - neg_accuracy)
